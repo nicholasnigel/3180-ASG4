ed0c2ee0f0d54ba849dc5f8a2292472c3d4e6cc2
Nigel Nicholas <nicholasnigel@yahoo.com>
2018-04-29 15:29:55 +0800

    header

--- asg4/logic.pl
+++ asg4/logic.pl
@@ -1,4 +1,20 @@
 /* Insert header here */
+%
+% CSCI3180 Principles of Programming Languages
+% 
+%  --- Declaration ---
+% 
+%  I declare that the assignment here submitted is original except for source
+%  material explicitly acknowledged. I also acknowledge that I am aware of
+%  University policy and regulations on honesty in academic work, and of the
+%  disciplinary guidelines and procedures applicable to breaches of such policy
+%  and regulations, as contained in the website
+%  http://www.cuhk.edu.hk/policy/academichonesty/
+% 
+%  Assignment 4	
+%  Name : Nigel Nicholas    
+%  Student ID : 1155088791
+%  Email Addr : nigel7@cse.cuhk.edu.hk
 
 /* 1a */
 uint_num(0).
--- asg4/task.pl
+++ asg4/task.pl
@@ -1,5 +1,23 @@
 /* Insert header here */
 
+%
+% CSCI3180 Principles of Programming Languages
+% 
+%  --- Declaration ---
+% 
+%  I declare that the assignment here submitted is original except for source
+%  material explicitly acknowledged. I also acknowledge that I am aware of
+%  University policy and regulations on honesty in academic work, and of the
+%  disciplinary guidelines and procedures applicable to breaches of such policy
+%  and regulations, as contained in the website
+%  http://www.cuhk.edu.hk/policy/academichonesty/
+% 
+%  Assignment 4	
+%  Name : Nigel Nicholas    
+%  Student ID : 1155088791
+%  Email Addr : nigel7@cse.cuhk.edu.hk
+
+
 task(t1, 2, 4).
 task(t2, 4, 6).
 task(t3, 3, 7).
